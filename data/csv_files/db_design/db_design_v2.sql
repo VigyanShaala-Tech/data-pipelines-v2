1e4dfb1be9f3a0d92b932228e5344bffce9ce60e
akshatasatpute <akshatasatpute1333@gmail.com>
2025-04-21 18:17:11 +0530

    Updated accelerator tables with the requested changes

--- data/csv_files/db_design/db_design_v2.sql
+++ data/csv_files/db_design/db_design_v2.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE "education_course" (
     "id" int NOT NULL,
-    "course_name" varchar,            -- name displayed in google form
-    "display_name" varchar,
+    "course_name" varchar(50),            -- name displayed in google form
+    "display_name" varchar(100),
     PRIMARY KEY ("id")
 );
 
@@ -11,7 +11,7 @@ CREATE TABLE "education_course" (
 CREATE TABLE "resource" (
     "id" int NOT NULL,
     "category" enum,                 -- category enum - pre-recorded,quiz,assignments,reference material.
-    "title" varchar,
+    "title" varchar(300),
     "description" text,
     "location" enum,
     "resource_link" text,
@@ -25,7 +25,7 @@ CREATE TABLE "payment_details" (
     "id" int,
     "role_id" int,                   -- refers to student_id and mentor_id
     "role_type" enum,                -- role_type - student, mentors.
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "paid_amount_inr" decimal,
     "fee_received_inr" decimal,
     "mode_of_payment" enum,          -- mode_of_payment - cash, cheque, online, others
@@ -52,9 +52,9 @@ CREATE TABLE "student_assignment" (
     "student_id" int,
     "resource_id" int,
     "mentor_id" int,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "submission_status" enum,   -- accepted, rejected, under-review
-    "marks" int,
+    "marks_pct" decimal,
     "feedback/comments" text,
     "submitted_at" timestamp,
     "assignment_file" bytea,    -- Needs to finalize storage location and file size 
@@ -68,9 +68,9 @@ CREATE TABLE "student_assignment" (
 
 
 CREATE TABLE "student_cohort" (
-    "student_code" varchar NOT NULL,     -- student_code :  year  + cohort_code + student_id e.g 24INC0010000001,24ACC0010000001
+    "student_code" varchar(15) NOT NULL,     -- student_code :  year  + cohort_code + student_id e.g 24INC0010000001,24ACC0010000001
     "student_id" int,
-    "cohort_code " varchar,
+    "cohort_code " varchar(6),
     "is_leader" boolean,
     "cohort_enroll_date" date,
     PRIMARY KEY ("student_code"),
@@ -82,13 +82,13 @@ CREATE TABLE "student_cohort" (
 
 CREATE TABLE "student_details" (
     "id" int NOT NULL,
-    "email" varchar,
-    "first_name" varchar,
-    "last_name" varchar,
+    "email" varchar(254),
+    "first_name" varchar(100),
+    "last_name" varchar(100),
     "gender" enum,         -- male, female and other
     "is_female" boolean,
     "is_indian" boolean,
-    "phone" varchar,       -- To Do- Phone number should be 10 digit.Once the student selects country from the drop down, the form should populate the country code automatically
+    "phone" varchar(15),       -- To Do- Phone number should be 10 digit in dbt.Once the student selects country from the drop down, the form should populate the country code automatically
     "date_of_birth" date,
     "caste" enum,         -- OBC, ST/SC, Others
     "annual_family_income_inr" text,     -- Needs to be converted to range. (should be in INR)'
@@ -100,8 +100,8 @@ CREATE TABLE "student_details" (
 
 
 CREATE TABLE "education_category" (
-    "id" varchar NOT NULL,
-    "category_name" varchar,
+    "id" int NOT NULL,
+    "category_name" varchar(100),
     PRIMARY KEY ("id")
 );
 
@@ -124,10 +124,10 @@ CREATE TABLE "mentor_registration_details" (
 
 CREATE TABLE "location" (
     "id" int NOT NULL,
-    "country" varchar,
-    "country_code" varchar,            --- like IN for India,AU for Australia,ZW for Zimbabwe
-    "state_union_territory" varchar,
-    "district" varchar,
+    "country" varchar(50),
+    "country_code" varchar(2),            --- like IN for India,AU for Australia,ZW for Zimbabwe
+    "state_union_territory" varchar(60),
+    "district" varchar(60),
     "location_category" enum,          -- Tier 1 city, Tier 2 city, Towns and Rural villages
     PRIMARY KEY ("id")
 );
@@ -136,11 +136,12 @@ CREATE TABLE "location" (
 
 CREATE TABLE "mentor_details" (
     "id" int,
-    "name" varchar,
-    "email_id" varchar,
-    "linkedIn_url" varchar,
-    "current_position" varchar,
-    "phone_number" varchar,
+    "name" varchar(50),
+    "email_id" varchar(254),
+    "linkedIn_url" varchar(200),
+    "current_position" varchar(100),
+    "phone_number" varchar(15),  ---can also be other country number hence assigned length 15
+    "mentor_role"  enum,         --- SUK role model, Accelerator Mentor, Research Project Developers
     "location_id" int,
     "joined_on" date,
     "is_active" boolean,
@@ -165,7 +166,7 @@ CREATE TABLE "student_mentor" (
     "id" int NOT NULL,
     "mentor_id " int,
     "student_id" int,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "assigned_at" timestamp,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("mentor_id") REFERENCES mentor_details("id"),
@@ -177,18 +178,18 @@ CREATE TABLE "student_mentor" (
 
 CREATE TABLE "subject" (
     "id" int NOT NULL,
-    "subject_name" varchar,
-    "display_name" varchar,     -- name displayed in google form.
+    "subject_name" varchar(100),
+    "display_name" varchar(150),     -- name displayed in google form.
     PRIMARY KEY ("id")
 );
 
 
 
 CREATE TABLE "cohort" (
-    "cohort_code" varchar NOT NULL,
-    "program_code" varchar,
+    "cohort_code" varchar(6) NOT NULL,    ---INC001,INC002,INC003,...
+    "program_code" varchar(3),   ---INC,STC,ACC
     "cohort_number" int,
-    "cohort_name" varchar,
+    "cohort_name" varchar(300),
     "type" enum,                 -- type - open (Nationwide, open to all STEM students. Science and/or Engineering) and curriculum (Focused cohorts only for students from colleges under Govt. Partnerships - Telangana/ UK.)
     "start_date" date,
     "end_date" date,
@@ -202,8 +203,8 @@ CREATE TABLE "cohort" (
 CREATE TABLE "college" (
     "id" int NOT NULL,
     "location_id" int,
-    "university" varchar,       -- university names
-    "college" varchar,           -- college names
+    "university" varchar(200),       -- university names
+    "college" varchar(200),           -- college names
     PRIMARY KEY ("id"),
     FOREIGN KEY ("location_id") REFERENCES location("id")
 );
@@ -213,7 +214,7 @@ CREATE TABLE "college" (
 CREATE TABLE "mentor_cohort" (
     "id" int NOT NULL,
     "mentor_id" int,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "cohort_enroll_date" date,
     "is_active" boolean,
     "started_on" timestamp,
@@ -264,7 +265,7 @@ CREATE TABLE "student_quiz" (
     "id" int NOT NULL,
     "student_id" int,
     "resource_id" int,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "max_marks" int,
     "reattempts" int,
     "attempted_at" timestamp,
@@ -278,7 +279,7 @@ CREATE TABLE "student_quiz" (
 
 CREATE TABLE "program" (
     "program_code" enum NOT NULL,          --- program_code- INC, ACC, STC
-    "full_form" varchar,
+    "full_form" varchar(20),
     "start_date" date,
     PRIMARY KEY ("program_code")
 );
@@ -299,7 +300,7 @@ CREATE TABLE "mentor_session" (
 CREATE TABLE "cohort_resource" (
     "id" int NOT NULL,
     "resource_id" int,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "marks_weightage" int,
     "created_at" timestamp,
     "updated_at" timestamp,
@@ -318,8 +319,8 @@ CREATE TABLE "referral_college_professor" (
     "id" int NOT NULL,
     "student_id" int,
     "college_id" int,
-    "name" varchar,
-    "phone" varchar,
+    "name" varchar(50),
+    "phone" varchar(15),
     PRIMARY KEY ("id"),
     FOREIGN KEY ("student_id") REFERENCES student_details("id"),
     FOREIGN KEY ("college_id") REFERENCES college("id")
@@ -329,7 +330,7 @@ CREATE TABLE "referral_college_professor" (
 
 CREATE TABLE "session" (
     "id" int NOT NULL,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "session_name" text,
     "type" enum,                    -- type-  masterclass, SUK, workshop
     "duration(in minutes)" int,
@@ -370,9 +371,9 @@ CREATE TABLE "student_pre_recorded" (
     "id" int NOT NULL,
     "student_id" int,
     "resource_id" int,
-    "cohort_code" varchar,
-    "watchtime" int,
-    "watched_on" bigint,
+    "cohort_code" varchar(6),
+    "watchtime_in_min" int,
+    "watched_at" timestamp,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("student_id") REFERENCES student_details("id"),
     FOREIGN KEY ("resource_id") REFERENCES resource("id"),
@@ -382,11 +383,12 @@ CREATE TABLE "student_pre_recorded" (
 
 CREATE TABLE "accelerator_project_details" (              
     "id" int NOT NULL,
-    "cohort_code" varchar,
-    "project_title" varchar,
+    "cohort_code" varchar(6),
+    "project_title" text,
     "description" text,
     "marks" int,
     "start_date" date,
+    "end_date" date,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("cohort_code") REFERENCES cohort("cohort_code")
 );
@@ -398,7 +400,7 @@ CREATE TABLE "accelerator_project_details_cost" (
     "project_details_id" int,
     "payment_details_id" int,
     "amount_inr" int,
-    "is_paid" boolean,
+    "paid_on" date,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("project_details_id") REFERENCES accelerator_project_details("id"),
     FOREIGN KEY ("payment_details_id") REFERENCES payment_details("id")
@@ -411,9 +413,9 @@ CREATE TABLE "accelerator_mentor_pre_recorded" (
     "id" int NOT NULL,
     "mentor_id" int,
     "resource_id" int,
-    "cohort_code" varchar,
-    "watchtime(duration in min)" int,
-    "watched_on" date,
+    "cohort_code" varchar(6),
+    "watchtime_in_min" int,
+    "watched_at" timestamp,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("mentor_id") REFERENCES mentor_details("id"),
     FOREIGN KEY ("resource_id") REFERENCES resource("id"),
@@ -427,9 +429,10 @@ CREATE TABLE "accelerator_mentor_project" (
     "id" int NOT NULL,
     "mentor_id" int,
     "project_details_id" int,
-    "cohort_code" varchar,
+    "cohort_code" varchar(6),
     "is_active" boolean,
     "dropout_reason" text,
+    "dropped_out_on" date,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("mentor_id") REFERENCES mentor_details("id"),
     FOREIGN KEY ("project_details_id") REFERENCES accelerator_project_details("id"),
@@ -445,12 +448,14 @@ CREATE TABLE "accelerator_student_project_performance" (
     "awareness_score" int,
     "attentiveness_score" int,
     "quality_score" int,
-    "tasks_assigned" text,
+    "max_score" int,
+    "tasks_assigned" text[],
     "theme" enum,           --- Week 1 :Project Initiation and Goal Setting, Week 2 :Project Planning and Development, Week 3:Review and Experimental Design(Capture attention)....
     "7E learning phase for this week" enum,   -- Elicit-Week 1-2, Engage -Week 3-5, Explore- Week 6-8, Explain -Week 9,....
     "is_active" boolean,
     "updated_at" timestamp,
     "dropout_reason" text,
+    "dropped_out_on" date,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("student_id") REFERENCES student_details("id"),
     FOREIGN KEY ("project_details_id") REFERENCES accelerator_project_details("id")
@@ -459,18 +464,17 @@ CREATE TABLE "accelerator_student_project_performance" (
 
 
 ---Need clarity regarding whether to keep the fields like interactions related to whatsapp as JSONB or not. For now have kept it as column fields.
-CREATE TABLE "accelerator_student_mentor_Interaction" (                
+CREATE TABLE "accelerator_student_mentor_interaction" (                
     "id" int NOT NULL,
     "student_id" int,
     "mentor_id" int,
     "project_details_id" int,
     "total_session" int,
     "average_duration" int,
-    "attended_sessions" int,
-    "interacted_on_whatsapp" boolean,    ---Yes,No       
+    "attended_sessions" int,     
     "number_of_whatsapp_interaction" int,   
     "quality_of_interaction" enum,           --- Excellent,Good,Fair,Poor
-    "week's interactions helped to progress towards project goals" enum,  ----Not at all,Somewhat,Yes, substantially
+    "weekly_interactions_project_progress" enum,  ----Not at all,Somewhat,Yes, substantially
     "updated_at" date,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("student_id") REFERENCES student_details("id"),
